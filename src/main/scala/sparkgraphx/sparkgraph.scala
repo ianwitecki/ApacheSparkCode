3bc7b0ea74387dc2fbfe51fc195d25d031759e3b
Ian Witecki <iwitecki@trinity.edu>
2017-11-14 02:17:30 -0600

    Significant progress on graphing

--- src/main/scala/sparkgraphx/sparkgraph.scala
+++ src/main/scala/sparkgraphx/sparkgraph.scala
@@ -21,17 +21,18 @@ object MedSet extends App {
  sc.setLogLevel("WARN")
 
 	val cits = descriptorReader("/data/BigData/Medline/medsamp2016a.xml")
-	var citationNodes = cits
+	//var citationNodes = cits
 
-	for (i <- 'b' to 'c') {
+	/*for (i <- 'b' to 'c') {
 		citationNodes = citationNodes ++ descriptorReader("/data/BigData/Medline/medsamp2016" + i + ".xml") 
-	}
+	}*/
 
-	//val nodesRDD = sc.parallelize(nodes.foreach(_.toString).toArray)	
+	val citationNodes = cits ++ descriptorReader("/data/BigData/Medline/medsamp2016" + "b" + ".xml") //++ descriptorReader("/data/BigData/Medline/medsamp2016" + "c" + ".xml") 
+	//IN CLASS CODE
 
-	val majorKeys = citationNodes.filter(n => (n \ "@MajorTopicYN").toString == "Y").map(_.text)
-	val citationSeq = citationNodes.map(_.text)
-
+	val majorKeys = citationNodes.flatten.filter(n => (n \ "@MajorTopicYN").toString == "Y").map(_.text)
+	val citationSeq = citationNodes.flatten.map(_.text)
+/*
 	//Problem 1
 	println("Number of Descriptors")
 	val count = citationSeq.distinct.count(_ => true)
@@ -52,24 +53,74 @@ object MedSet extends App {
 	//Problem 4
 	println("Math: Number of Pairs")
 	println((count * (count - 1)) / 2)
-
+*/
 
 	//Problem 5
-	println("Number of Pairs")
+	val citPairSeq = citationNodes.map(seq => seq.map(node => node.text))
+	.flatMap{seq =>
+		seq.combinations(2)
+	}
+	citPairSeq.take(10) foreach println
+	println(citPairSeq.distinct.count(_ => true))
+
+
+	/*println("Number of Pairs")
 	val citEdges = citRDD.flatMap(citation => citation.combinations(2))
-	println(citEdges.count)
+	println(citEdges.count)*/
 
 	//Problem 6
 	/* The graph of pair terms should not be directed becuase there will never be case where a descriptor has an edge to another descriptor, but there is no edge from that the destination descriptor back. This implies that our graph is only interested in how related different descriptors are to each other*/
 
+	// OUT OF CLASS CODE 
+	
+	//ALL DESCRIPTORS
+	val regNodeIndexPairs = citationSeq.distinct.zipWithIndex.map { case (n, i) => i.toLong -> n }
+
+	println("Nodes Done")
+	regNodeIndexPairs.take(5) foreach println
+
+	val indexMap = regNodeIndexPairs.map { case (i, n) => n -> i }.toMap
 
 
-def descriptorReader(path: String): NodeSeq = {
+	println("Index Map")
+
+	val regEdgePairs = citPairSeq.distinct.flatMap{ list => 
+		Seq( Edge(indexMap(list(0)), indexMap(list(1)), ()), 
+			 Edge(indexMap(list(1)), indexMap(list(0)), ()))
+	}
+
+	regEdgePairs.take(5) foreach println
+
+
+	println("Edges Done")
+
+	val	regNodeRDD = sc.parallelize(regNodeIndexPairs)
+	println("NodeRDD")
+	val regEdgeRDD = sc.parallelize(regEdgePairs)
+	println("EdgeRDD")
+	val regGraph = Graph(regNodeRDD, regEdgeRDD)
+	println("GraphRDD")
+
+	println(regGraph.numEdges)
+	println(regGraph.numVertices)
+
+	// Connected Components
+	println("Connected Components")
+	println(regGraph.connectedComponents().vertices.map(_._2).distinct.count(_=>true))
+
+	//Top Words By Page Rank
+
+
+
+
+	//MAJOR DESCRIPTORS
+
+def descriptorReader(path: String): Seq[NodeSeq] = {
 	val parsed = xml.XML.loadFile(path)
-	val cit = (parsed \ "MedlineCitation" \\ "DescriptorName")
+	val cit = (parsed \ "MedlineCitation").map(x => (x \\ "DescriptorName"))
 	return cit
 }
 
 sc.stop
 
-}
+}	
