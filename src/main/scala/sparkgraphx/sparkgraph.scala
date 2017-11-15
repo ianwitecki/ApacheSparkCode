9ad4b12baa3b202fb65cfe0a6496d2b25da837e3
Ian Witecki <iwitecki@trinity.edu>
2017-11-14 22:27:58 -0600

    finished answering all problems for graphs

--- src/main/scala/sparkgraphx/sparkgraph.scala
+++ src/main/scala/sparkgraphx/sparkgraph.scala
@@ -9,9 +9,22 @@ import org.apache.spark.graphx.lib.ShortestPaths
 import scala.xml._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+import org.apache.spark.graphx.lib.ShortestPaths
+import scalafx.application.JFXApp
+import scalafx.Includes._
+import scalafx.scene.Scene
+import scalafx.scene.chart.ScatterChart
+import scalafx.scene.chart.NumberAxis
+import scalafx.scene.chart.XYChart
+import scalafx.scene.layout.TilePane
+import scalafx.collections.ObservableBuffer
+import swiftvis2.plotting._
+import swiftvis2.plotting.renderer.FXRenderer
 
 
-object MedSet extends App {
+
+
+object MedSet extends JFXApp {
 
  Logger.getLogger("org").setLevel(Level.OFF)
 
@@ -27,17 +40,17 @@ object MedSet extends App {
 		citationNodes = citationNodes ++ descriptorReader("/data/BigData/Medline/medsamp2016" + i + ".xml") 
 	}*/
 
-	val citationNodes = cits ++ descriptorReader("/data/BigData/Medline/medsamp2016" + "b" + ".xml") //++ descriptorReader("/data/BigData/Medline/medsamp2016" + "c" + ".xml") 
+	val citationNodes = cits ++ descriptorReader("/data/BigData/Medline/medsamp2016" + "b" + ".xml") ++ descriptorReader("/data/BigData/Medline/medsamp2016" + "c" + ".xml") 
 	//IN CLASS CODE
 
 	val majorKeys = citationNodes.flatten.filter(n => (n \ "@MajorTopicYN").toString == "Y").map(_.text)
 	val citationSeq = citationNodes.flatten.map(_.text)
-/*
+
 	//Problem 1
 	println("Number of Descriptors")
 	val count = citationSeq.distinct.count(_ => true)
 	println(count)
-
+/*
 	//Problem 2
 	println("All Descriptors")
 	val citRDD = sc.parallelize(citationSeq)
@@ -56,12 +69,12 @@ object MedSet extends App {
 */
 
 	//Problem 5
-	val citPairSeq = citationNodes.map(seq => seq.map(node => node.text))
+	val citPairSeq = sc.parallelize(citationNodes.map(seq => seq.map(node => node.text)))
 	.flatMap{seq =>
 		seq.combinations(2)
 	}
 	citPairSeq.take(10) foreach println
-	println(citPairSeq.distinct.count(_ => true))
+	println(citPairSeq.distinct.count())
 
 
 	/*println("Number of Pairs")
@@ -76,43 +89,58 @@ object MedSet extends App {
 	//ALL DESCRIPTORS
 	val regNodeIndexPairs = citationSeq.distinct.zipWithIndex.map { case (n, i) => i.toLong -> n }
 
-	println("Nodes Done")
-	regNodeIndexPairs.take(5) foreach println
-
 	val indexMap = regNodeIndexPairs.map { case (i, n) => n -> i }.toMap
 
-
-	println("Index Map")
 
 	val regEdgePairs = citPairSeq.distinct.flatMap{ list => 
 		Seq( Edge(indexMap(list(0)), indexMap(list(1)), ()), 
 			 Edge(indexMap(list(1)), indexMap(list(0)), ()))
 	}
 
-	regEdgePairs.take(5) foreach println
-
-
-	println("Edges Done")
 
 	val	regNodeRDD = sc.parallelize(regNodeIndexPairs)
-	println("NodeRDD")
-	val regEdgeRDD = sc.parallelize(regEdgePairs)
-	println("EdgeRDD")
-	val regGraph = Graph(regNodeRDD, regEdgeRDD)
-	println("GraphRDD")
+	val regGraph = Graph(regNodeRDD, regEdgePairs)
 
-	println(regGraph.numEdges)
 	println(regGraph.numVertices)
 
 	// Connected Components
-	println("Connected Components")
-	println(regGraph.connectedComponents().vertices.map(_._2).distinct.count(_=>true))
+	println("Connected Components \n")
+//	println(regGraph.connectedComponents().vertices.map(_._2).distinct.count())
 
 	//Top Words By Page Rank
+	println("Page Rank \n")
+//	regGraph.pageRank(0.01).vertices.sortBy(_._2, ascending = false).take(10).map(a => (a._2, regNodeIndexPairs((a._1).toInt))) foreach println
+
+	//  Degree Distribution
+	println("Degree Distribution \n")
+	val degreeDist = regGraph.degrees.sortBy(_._1)
+
+//	val bins = degreeDist.map(a => (a._1).toDouble).collect()
+	println(count)
+	println(degreeDist.count)
+	degreeDist.take(5) foreach println
+	println()
+	degreeDist.sortBy(_._1, false).take(5) foreach println
+
+	val bins = (0.0 to count.toDouble by 10.0).toArray
+	val y = degreeDist.map(_._1.toDouble).collect
+    val hist = degreeDist.map(_._2.toDouble).histogram(y, true)
+    val plot = Plot.histogramPlot(bins, hist, RedARGB, true, "Degree Distribution of All Descriptors", "Vertex", "Degree Distribution")
+     FXRenderer(plot)
 
 
+	//	Shortest Path
+/*
+	val pregnancySP = ShortestPaths.run(regGraph, Seq(indexMap("Esophagus")))
+    println(pregnancySP.vertices.filter(_._1==indexMap("Pregnancy")).first)
 
 
+	val arterySP = ShortestPaths.run(regGraph, Seq(indexMap("Femoral Artery")))
+    println(arterySP.vertices.filter(_._1==indexMap("Electroencephalography")).first)
+
+	val taxesSP = ShortestPaths.run(regGraph, Seq(indexMap("Taxes")))
+    println(taxesSP.vertices.filter(_._1==indexMap("Guinea Pigs")).first)
+*/
 	//MAJOR DESCRIPTORS
 
 def descriptorReader(path: String): Seq[NodeSeq] = {
@@ -123,4 +151,4 @@ def descriptorReader(path: String): Seq[NodeSeq] = {
 
 sc.stop
 
-}	
+}
